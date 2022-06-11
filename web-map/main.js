9452682161242a3067cfe57f45e8240672f59d91
Sergey Leschina <mail@putnik.tech>
2022-06-11 18:48:50 +0300

    HeatMap, clusters, district selection

--- web-map/main.js
+++ web-map/main.js
@@ -1,9 +1,9 @@
-const map = L.map('map').setView([55.5844, 37.6536], 15);
-
+const map = L.map('map');
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+map.setView([55.75, 37.61], 14);
 
 function fetchJSON(url) {
   return fetch(url)
@@ -21,45 +21,67 @@ const pointStyle = (color) => { return {
   fillOpacity: 0.7
 }};
 
-window.districts = new Set();
+let features = [];
+let markersLayer;
 
-function loadDisctrict(district) {
-
-  function addGeoJSON(path, style) {
-    console.log(path, "is loading")
-
-    fetchJSON(path).then(data => {  
-
-      if (window.districts.size == 0)
-      { data['features'].forEach((f) => window.districts.add(f.properties.Attributes.District)) }
-
-      data['features'].filter(f => f.properties.Attributes.District == district).forEach(
-        feature => L.geoJSON
-        ( feature
-        , {pointToLayer:  (feature, latlng) => L.circleMarker(latlng, style)}
-        ).bindPopup('<code>'+JSON.stringify(feature.properties.Attributes, undefined, 2)+'</code>').addTo(map)
-      )
-      console.log(path, "is loaded")
+function loadDistrictList() {
+  return fetchJSON('./data/districts.json').then(data => {
+    const districtSelector = document.getElementById('district-selector' );
+    data.map(function(row) {
+      const option = document.createElement('option');
+      option.value = row;
+      option.textContent = row;
+      districtSelector.appendChild(option);
     })
-  }
-  
-  function addGeoJSON2(path, style) {
-    console.log(path, "is loading")
-    fetchJSON(path).then(function(data) {
-      data['features'].filter(f => f[1].properties.District == district).forEach(feature => L.geoJSON(feature[1],
-      {pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, style);
-      }}).bindPopup('<code>'+JSON.stringify(feature[1].properties, undefined, 2)+'</code>').addTo(map))
-      console.log(path, "is loaded")
-    })
-  }
-
-  addGeoJSON('./data/mass.geojson', pointStyle("#00ff00"))
-  addGeoJSON('./data/dvor.geojson', pointStyle("#ff7800"))
-  addGeoJSON2('./data/podezd.geojson', pointStyle("#0078ff"))
-
+    districtSelector.onchange = function selectDistrict(event) {
+      loadDistrict(event.target.value);
+    };
+  })
 }
 
-window.loadDisctrict = loadDisctrict
+function loadPoints() {
+  return fetchJSON('./data/features.geojson').then(function(data) {
+    features = data['features'];
+  });
+}
 
-window.printDistricts = () => {for (let d of window.districts) {console.log(d)}}
+function generateHeatMap() {
+  L.heatLayer(
+    features.map(feature => [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]),
+    {radius: 23}
+  ).addTo(map);
+}
+
+function loadDistrict(district) {
+  if (markersLayer) {
+    map.removeLayer(markersLayer);
+  }
+  markersLayer = L.markerClusterGroup({
+    spiderfyOnMaxZoom: false,
+    showCoverageOnHover: false,
+    zoomToBoundsOnClick: false,
+    maxClusterRadius: 40
+  });
+  const districtFeatures = features.filter(feature => feature.properties.District === district)
+  let latMin, latMax, lngMin, lngMax;
+  districtFeatures.forEach(function (feature) {
+    L.geoJSON(feature, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, pointStyle("#0078ff"));
+      }
+    }).addTo(markersLayer)
+    const [x, y] = feature.geometry.coordinates;
+    latMin = latMin && latMin < x ? latMin : x;
+    latMax = latMax && latMax > x ? latMax : x;
+    lngMin = lngMin && lngMin < y ? lngMin : y;
+    lngMax = lngMax && lngMax > y ? lngMax : y;
+  });
+  map.setView([(lngMin + lngMax) / 2, (latMin + latMax) / 2], 14);
+  markersLayer.addTo(map);
+}
+
+window.init = function () {
+  return loadPoints()
+    .then(generateHeatMap)
+    .then(loadDistrictList);
+}
